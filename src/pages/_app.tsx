3142cabdf90493241e6e107e77af476833858950
SadSAndWiCh _ <caiocamargo007@gmail.com>
2023-01-25 14:41:34 -0300

    feat: import roboto font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,23 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import { Roboto } from '@next/font/google';
+
+const roboto = Roboto({
+  weight: ['400', '500', '700'],
+  subsets: ['latin'],
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <style jsx global>
+        {`
+          :root {
+            --roboto-font: ${roboto.style.fontFamily};
+          }
+        `}
+      </style>
+      <Component {...pageProps} />
+    </>
+  )
 }
