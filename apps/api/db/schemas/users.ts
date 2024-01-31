52079888d20ccf1e46bd74c45750ad8c9ca9eeb3
sads4ndwich <caiocamargo007@gmail.com>
2024-01-30 23:40:12 -0300

    feat(database): include email and password

--- apps/api/db/schemas/users.ts
+++ apps/api/db/schemas/users.ts
@@ -3,4 +3,6 @@ import { sqliteTable, text } from "drizzle-orm/sqlite-core";
 export const users = sqliteTable("users", {
   id: text("id").notNull().primaryKey(),
   username: text("username").notNull(),
+  email: text("email").notNull().unique(),
+  hashedPassword: text("hashed_password").notNull()
 });
